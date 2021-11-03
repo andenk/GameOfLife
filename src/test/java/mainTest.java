093c62b985217ef765f7482f0ec080a2be11da02
andreas nehl <andenk@hotmail.se>
2021-11-03 11:22:36 +0100

    ny array har inget liv i hörnen

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -1,7 +1,52 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class mainTest {
 
+    main main = new main();
 
+    int x = 8;
+    int y = 8;
+    int[][] ControllOfSpecificValue;
+
+    int[][] inputValus = {{1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1}
+
+    };
+    int[][] testGeneral = {{0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 1, 0, 0, 0},
+            {0, 0, 0, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+
+    };
+
+    @Test
+    public void LifeNotExistAroundEdges() {
+        // all edges ar dead
+
+        ControllOfSpecificValue = main.newGeneration(inputValus, y, x);
+        x = x - 1;
+        y = y - 1;
+        //check old inputArray that edges is alive
+        assertEquals(1,inputValus[0][0]);
+        assertEquals(1,inputValus[y][x]);
+
+        // check that the new array edges is dead
+        assertEquals(0, ControllOfSpecificValue[0][0]);
+        assertEquals(0, ControllOfSpecificValue[0][x]);
+        assertEquals(0, ControllOfSpecificValue[y][0]);
+        assertEquals(0, ControllOfSpecificValue[y][x]);
+    }
 
 }
